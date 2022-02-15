eaaabb8324cd204211481f85c8d6a2e4742b4a54
Priyanka <priyankadombale27@gmail.com>
2022-02-15 22:36:36 +0530

    Added: Check_Employee_Present_OR_Not

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -3,44 +3,18 @@ package com.blz;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		int wagePerHour = 20;
-		int fullDayHour = 8;
-		int partTimeHour = 4;
-		int workingDaysInMonth = 20;
-		int wagePerDay;
-		int totalWage = 0;
-		int totalWorkingHour = 0;
-		int totalWorkingDay = 0;
-
-		for (int i = 1; i <= workingDaysInMonth; i++) {
-			if (totalWorkingHour <= 100 && totalWorkingDay <= 20) {
-				int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-
-				if (empCheck == 2) {
-					wagePerDay = (wagePerHour * fullDayHour);
-					totalWage = totalWage + wagePerDay;
-					totalWorkingHour = totalWorkingHour + fullDayHour;
-					totalWorkingDay++;
-				}
-
-				else if (empCheck == 1) {
-
-					wagePerDay = (wagePerHour * partTimeHour);
-					totalWage = totalWage + wagePerDay;
-					totalWorkingHour = totalWorkingHour + partTimeHour;
-					totalWorkingDay++;
-				} else {
-					wagePerDay = 0;
-					totalWage = totalWage + wagePerDay;
-				}
-
-			} else
-				break;
-		}
-
-		System.out.println("Total working hour = " + totalWorkingHour);
-		System.out.println("Toatl working day = " + totalWorkingDay);
-		System.out.println("Total wage of the month = " + totalWage);
+		System.out.println("Welcome in Employee Wage Computation..!!");
+		checkEmplPresence();
 	}
 
+	public static void checkEmplPresence() {
+
+		int randomNum = (int) (Math.random() * 2); // create random number using math function
+
+		if (randomNum == 1) // Check Employee presence
+		{
+			System.out.println(" Employee is Present " + randomNum);
+		} else
+			System.out.println(" Employee is Absent " + randomNum);
+	}
 }
