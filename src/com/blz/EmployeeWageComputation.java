f10edfb90a5dbb548be720d0292deb323c9f0076
Priyanka <priyankadombale27@gmail.com>
2022-02-15 23:32:36 +0530

    Added: Check_Employee_Full_Time_Salary_Logic

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -2,19 +2,28 @@ package com.blz;
 
 public class EmployeeWageComputation {
 
+	static int wagePerHour = 20;
+	static int empHrs = 0;
+	static int empHrsf = 8;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome in Employee Wage Computation..!!");
-		checkEmplPresence();
+
+		checkEmpSalary();
 	}
 
-	public static void checkEmplPresence() {
-
+	public static void checkEmpSalary() {
 		int randomNum = (int) (Math.random() * 2); // create random number using math function
 
 		if (randomNum == 1) // Check Employee presence
 		{
-			System.out.println(" Employee is Present " + randomNum);
-		} else
-			System.out.println(" Employee is Absent " + randomNum);
+			System.out.println(" Employee is Present ");
+			int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
+			System.out.println("Employee salary of Full time is :" + salary);
+		} else {
+			System.out.println(" Employee is Absent ");
+			int salary = wagePerHour * empHrs; // Employee Full timeSalary/Day
+			System.out.println("Employee salary is :" + salary);
+		}
 	}
 }
