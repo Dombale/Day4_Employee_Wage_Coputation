e910d95d9ebb1c6d824f4c9be6197a12872666a8
Priyanka <priyankadombale27@gmail.com>
2022-02-15 08:14:59 +0530

    Added:Welcome message

---
+++ src/com/blz/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.blz;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome in Employee Wage Computation....!!");
+	}
+
+}
